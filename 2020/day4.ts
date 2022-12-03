377ca05e805236d60d69f68e241e27d3c044df37
Nathan Fritch <nbfritch@gmail.com>
2022-12-03 10:46:46 -0600

    2020 day 4

--- 2020/day4.ts
+++ 2020/day4.ts
@@ -1,7 +1,8 @@
 const requiredFields = ['ecl', 'hcl', 'byr', 'iyr', 'eyr', 'pid', 'hgt'];
 
 const input = (await Deno.readTextFile('inputs/2020/day4')).split('\n\n').map(
-  (line) => line.replace('\n', ' ').split(' ').filter((x) => x.length > 0),
+  (chunk) =>
+    chunk.replaceAll('\n', ' ').split(' ').filter((x) => x.trim().length > 0),
 );
 
 const part1 = (puzzle: Array<Array<string>>): number => {
@@ -10,8 +11,49 @@ const part1 = (puzzle: Array<Array<string>>): number => {
   ).filter((x) => x == true).length;
 };
 
+const validate = (field: string): boolean => {
+  const [tag, value] = field.split(':');
+  switch (tag) {
+    case 'byr':
+      const byear = parseInt(value);
+      return byear >= 1920 && byear <= 2002;
+    case 'iyr':
+      const iyear = parseInt(value);
+      return iyear >= 2010 && iyear <= 2020;
+    case 'eyr':
+      const eyear = parseInt(value);
+      return eyear >= 2020 && eyear <= 2030;
+    case 'cid':
+      return true;
+    case 'ecl':
+      return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(value);
+    case 'hcl':
+      const validChars = '0123456789abcdef';
+      return value.length === 7 && value.charAt(0) === '#' &&
+        [...value.slice(1)].every((c) => validChars.includes(c));
+    case 'pid':
+      return value.length === 9;
+    case 'hgt':
+      if (value.includes('cm')) {
+        const cm = parseInt(value.replace('cm', ''));
+        return cm >= 150 && cm <= 193;
+      } else {
+        const inc = parseInt(value.replace('in', ''));
+        return inc >= 59 && inc <= 76;
+      }
+    default:
+      return false;
+  }
+};
+
 const part2 = (puzzle: Array<Array<string>>): number => {
-  return 0;
+  return puzzle.filter((line) => {
+    if (requiredFields.every((r) => line.some((field) => field.includes(r)))) {
+      return line.every((field) => validate(field));
+    } else {
+      return false;
+    }
+  }).length;
 };
 
 console.log(`2020-4-1 ${part1(input)}`);
